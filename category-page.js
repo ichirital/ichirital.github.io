a6d624f7d15ad7d3298fb6c4a311b03e3802744a
Irina Chirita <chirita@adobe.com>
2024-11-13 21:36:07 +0000

    Displays all the products returned by the API but needs proper CSS layout / formatting

--- category-page.js
+++ category-page.js
@@ -9,16 +9,19 @@ function getAllProducts() {
         .catch((err) => {
             console.log("Error - ", err)
         });
-
 }
 
-function displayProductList(json) {
-    console.log(json)
+function displayProductList(products) {
+    const productList = document.getElementById('product-list');
+    products.forEach(product => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+          <img id="product-image" src="${product.image}" alt="Product Image" width="300px">
+          <p><b><span id="product-title">${product.title}</span></b></p>
+          <p><span id="product-price">$${product.price}</span></p>
+          <p><input type="hidden" id="product-id" value="${product.id}"></p>
+          `;
+        productList.appendChild(li);
+      });
 
-    let productInfo = json[0]
-    document.getElementById("product-id").value = productInfo.id;
-    document.getElementById("product-title").textContent = productInfo.title;
-    document.getElementById("product-price").textContent = productInfo.price + "$";
-    document.getElementById("product-image").src = productInfo.image;
-    
 }
